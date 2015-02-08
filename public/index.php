7714c56259fdf11e1c03228508e913b127f29b1c
CJ <lim@chernjie.com>
2015-02-08 22:51:28 +0800

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,94 @@
+<?php
+
+if (array_key_exists('url', $_GET)
+	&& ! empty ($_GET['url']))
+{
+	if (empty($_COOKIE['reddit_session']))
+	{
+		header('HTTP/1.1 500 Please Login');
+		exit();
+	}
+	header('Content-Type: application/json');
+	$url = 'https://www.reddit.com' . $_GET['url'];
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_COOKIE, 'reddit_session=' .$_COOKIE['reddit_session']);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	// curl_setopt($curl, CURLOPT_HEADER, true);
+	$response = curl_exec($curl);
+	strlen($response) < 10 && header('HTTP/1.1 500 Please Login');
+	exit($response);
+}
+if (array_key_exists('username', $_POST)
+	&& ! empty ($_POST['username']))
+{
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, 'https://www.reddit.com/api/login');
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array(
+		'api_type'=> 'json',
+		'passwd'  => $_POST['password'],
+		'rem'     => 0,
+		'user'    => $_POST['username']
+	)));
+	// curl_setopt($curl, CURLOPT_HEADER, true);
+	$response = curl_exec($curl);
+	$response = json_decode($response, true);
+	setcookie('reddit_session', $response['json']['data']['cookie']);
+	// exit($response);
+}
+?>
+<html>
+	<head>
+		<title>Gallery</title>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> <!-- 33 KB -->
+		<!-- fotorama.css & fotorama.js. -->
+		<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.js"></script> <!-- 16 KB -->
+	</head>
+	<body style="background-color: #A6A6A6;">
+		<div class="fotorama"></div>
+		<div class="login" style="display:none;">
+			<form method="post" action="">
+				<input type="text" placeholder="Username" name="username" />
+				<input type="password" placeholder="Password" name="password" />
+				<input type="submit">
+			</form>
+		</div>
+		<script type="text/javascript">
+			var topic = document.location.hash.replace('#', '') || 'fotorama';
+			$.ajax({
+				url: '?url=/user/me/liked.json%3Flimit%3D100',
+				success: function (data) {
+					var urls = [];
+					$.each(data.data.children, function(i, el) {
+						urls.push({img:el.data.url, caption:el.data.title});
+					});
+					var fotorama = $('.fotorama').fotorama({
+						allowfullscreen: 'native',
+						transition: 'crossfade',
+						loop: true,
+						autoplay: true,
+						shuffle: true,
+						data: urls,
+					}).data('fotorama');
+					window.fotorama = fotorama;
+					// var o_nativeFullScreen = 1;
+					fotorama.toggleFullScreen();
+				},
+				error: function (jqXhr, textStatus, errorThrown) {
+					if (errorThrown == 'Please Login') {
+						$('.fotorama').html(errorThrown);
+						$('.login').show();
+					}
+				},
+				complete: function (jqXHR, textStatus) {
+					// run again 
+				}
+			});
+		</script>
+	</body>
+</html>
